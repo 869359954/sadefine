4c728199316a97331f3610e1924ee829d5afd472
梁爽 <liangshuang@sensorsdata.cn>
2020-03-02 20:07:14 +0800

    h

--- defineindex.js
+++ defineindex.js
@@ -41,12 +41,12 @@
             sensors.quick('autoTrack');
             sensors.track('lstest',{});
 
-            window.SensorsData_APP_JS_Bridge = {
-                sensorsdata_define_mode : function(data){
+            // window.SensorsData_APP_JS_Bridge = {
+            //     sensorsdata_define_mode : function(data){
                 
-                },
-                sensorsdata_track:function(data){
-                    console.log(data);
-                }
-            };
+            //     },
+            //     sensorsdata_track:function(data){
+            //         console.log(data);
+            //     }
+            // };
             
