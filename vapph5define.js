72819316b3c7c5df8630312b2a8e99dc92cd966e
梁爽 <liangshuang@sensorsdata.cn>
2020-03-03 16:52:35 +0800

    f

--- vapph5define.js
+++ vapph5define.js
@@ -103,24 +103,26 @@
             subtree: true,
             attributes: true
         };
-        var obj = {
-            temp: null,
-            callback: callback,
-            func: function(){
-                clearTimeout(this.temp);
-                var that = this;
-                this.temp = setTimeout(function(){
-                    that.callback();
-                },800);
-            }
-        };
-        observe(document.body, options, function(){
-            console.log('监听到变化');
-            obj.func();
-        });
-        sdStore._.addEvent(window,'scroll', function () {
-            obj.func();
-        });
+        // var throttle = function(func, delay) {            
+        //     var timer = null;            
+        //     return function() {                
+        //         var context = this;               
+        //         var args = arguments;                
+        //         if (!timer) {                    
+        //             timer = setTimeout(function() {                        
+        //                 func.apply(context, args);                        
+        //                 timer = null;                    
+        //             }, delay);                
+        //         }            
+        //     };        
+        // };               
+        // observe(document.body, options, function(){
+        //     console.log('监听到变化');
+        //     throttle(callback,1000);
+        // });
+        // sdStore._.addEvent(window,'scroll', function () {
+        //     throttle(callback,1000);
+        // });
     }
     var sdStore = null;
     window.sa_jssdk_app_define_mode = function(sd){
